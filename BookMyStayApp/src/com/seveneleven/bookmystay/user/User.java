9a90fc50df80dd6af7c2bbbaf4076d51ecad453b
1234Sree0707 <sreerammpm@gmail.com>
2026-03-07 09:36:16 +0530

    Displays available rooms

--- BookMyStayApp/src/com/seveneleven/bookmystay/user/User.java
+++ BookMyStayApp/src/com/seveneleven/bookmystay/user/User.java
@@ -1,12 +1,13 @@
 package com.seveneleven.bookmystay.user;
-import  com.seveneleven.bookmystay.admin.HotelAdmin;
-import  com.seveneleven.bookmystay.service.InventoryService;
-
-
+import com.seveneleven.bookmystay.admin.HotelAdmin;
+import com.seveneleven.bookmystay.service.InventoryService;
+import com.seveneleven.bookmystay.service.Search;
 public class User {
 	public static void main(String[] args) {
-		HotelAdmin administrator=new HotelAdmin();
-		administrator.setInventory();
-		
-	}
+    	InventoryService inventory = new InventoryService();
+        HotelAdmin admin = new HotelAdmin(inventory);
+        admin.setInventory();
+        Search search = new Search(inventory);
+        search.searchRoomDetails();
+    }
 }
